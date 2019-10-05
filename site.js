bf49cd393f05dcdf06d9d3148993d797783f5ef3
Nandana1999 <Nandanan1999>
2019-10-06 02:17:41 +0530

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
 
+function toExcel() {
+
+    var XLSX = require('xlsx'),
+request = require('request');
+var fs = require('fs');
+var jsonfile = require('jsonfile')
+
+var file = 'data.json'
+
+var buf = fs.readFileSync("toExcel.xlsx");
+var wb = XLSX.read(buf, {type:'buffer'});
+
+console.log(wb.Sheets);
+
+jsonfile.writeFile(file, wb.Sheets, function (err) {
+  console.error(err)
+})
+    }
